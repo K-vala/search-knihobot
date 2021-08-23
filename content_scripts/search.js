25c640c7ae6f6f21d17f7306790f3d0236bf896e
K-vala <62719844+K-vala@users.noreply.github.com>
2021-08-23 16:56:02 +0000

    fix after html edit

--- content_scripts/search.js
+++ content_scripts/search.js
@@ -12,22 +12,24 @@
   /**
    * Listen for messages from the background script.
   */
+  const $xOne = xp =>
+  document.evaluate(
+    xp, document, null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE, null
+  ).singleNodeValue;
+
   browser.runtime.onMessage.addListener((message) => {
 
-    const rootElement = document.documentElement;
-    const body = rootElement.childNodes[2];
-    var innerText = body.children[3].innerText;
     if (message.searchTerm === "author") {
-      var authorName = innerText.substr(innerText.indexOf("Autor")+7,innerText.substr(innerText.indexOf("Autor")+7,innerText.length).indexOf("\n"));
+      var authorName = $xOne("/html/body/main/section/section/div[2]/table/tbody/tr[4]/td[2]").textContent.replace(/^\s+|\s+$/g,'');
       if (authorName.indexOf(",") > -1 ) {
         authorName = authorName.substr(authorName.indexOf(",")+2, authorName.length)+" "+authorName.substr(0, authorName.indexOf(","));
         }
       window.open("https://www.databazeknih.cz/search?q="+authorName+"&in=authors", "_blank");
     } else {
-      var bookName = innerText.substr(innerText.indexOf("Název")+7,innerText.substr(innerText.indexOf("Název")+7,innerText.length).indexOf("\n"));
+      var bookName = $xOne("/html/body/main/section/section/div[2]/table/tbody/tr[3]/td[2]").textContent.replace(/^\s+|\s+$/g,'');
       window.open("https://www.databazeknih.cz/search?q="+bookName, "_blank");
     }
   });
 
 })();
-
